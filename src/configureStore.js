d7a766731d2c27476740dbe0e3068bf02aceac47
Supasan Suklim <supasan.game@gmail.com>
2020-11-03 14:12:33 +0700

    Setup redux-devtools

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,13 +1,19 @@
 import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducers from './reducers'
 
 const middlewares = [thunk]
 
+// const store = createStore(
+//   reducers,
+//   applyMiddleware(...middlewares)
+// )
+
 const store = createStore(
-  reducers,
-  applyMiddleware(...middlewares)
-)
+  reducers, composeWithDevTools(
+  applyMiddleware(...middlewares),
+));
 
 export default store
