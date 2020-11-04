67864477ab47b87cef91117a2b090da899d016e7
Supasan Suklim <supasan.game@gmail.com>
2020-11-04 14:08:25 +0700

    Add subreddit loading

--- src/features/subreddit/SubredditList.js
+++ src/features/subreddit/SubredditList.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import List from '@material-ui/core/List'
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import SubredditItem from './SubredditItem'
 import { fetchSubredditIfNeeded } from './actions'
@@ -19,6 +20,8 @@ function SubredditList() {
   return (
     <List>
       {
+        subreddit.isFetching ?
+        <CircularProgress style={{ display: 'block', margin: 'auto' }} color="secondary" /> :
         subreddit.value.map((value, index) => (
           <SubredditItem text={value.data.title} to={value.data.display_name} key={index} />
         ))
